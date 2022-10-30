2710d867cb153c476491ae87864a06d7cfb110ed
Hamim Ahmed <106913071+Hamim-200@users.noreply.github.com>
2022-10-31 01:19:20 +0600

    Add files via upload

---
+++ C++ Codes/keyword_variable_datatype.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    //variable declare
+    int num1;
+    int num2;
+
+
+    //variable initialize
+    num1 =15;
+    num2= 30;
+
+    //declare and initialize in one line (dynamic initialization)
+    int num3=60,num4=90;
+
+    //Double data type
+    double num5 = 10.8,num6=20.12;
+
+    //Print
+    cout<< num1<<endl;
+    cout<< num2 <<endl;
+
+    cout<< "Number 3 = " <<num3 <<endl<< "Number 4 = " << num4 <<endl;
+    cout<< "Number 5 = " <<num5 <<endl<< "Number 6 = " << num6;
+
+
+    getch();
+}
+
