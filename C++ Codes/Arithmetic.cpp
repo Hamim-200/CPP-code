4ade2803202f99d6ff3ad339d7adb41cb9c7edab
Hamim Ahmed <106913071+Hamim-200@users.noreply.github.com>
2022-11-04 02:29:14 +0600

    Day-4

---
+++ C++ Codes/Arithmetic.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+int main()
+{
+    int num1,num2;
+    num1 = 100;
+    num2 = 50;
+
+    int sum = num1 + num2;
+    cout <<"Sum is = " <<sum;
+    cout<<endl;
+
+    int sub = num1 - num2;
+    cout <<"subtraction is = " <<sub;
+    cout<<endl;
+
+    int mul = num1 * num2;
+    cout <<"Multiplication is = " <<mul;
+    cout<<endl;
+
+
+    int div = num1 / num2;
+    cout <<"division is = " <<div;
+    cout<<endl;
+
+
+    int mod = num1 % num2;
+    cout <<"modulus is = " <<mod;
+    cout<<endl;
+
+
+
+
+
+
+
+}
+
---
+++ C++ Codes/user_input.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+int main()
+{
+        double num;
+        char ch;
+        char name[20];
+
+
+        cout << "Enter a number : ";
+        cin>>num;
+
+        cout << "Enter a Character : ";
+        cin>>ch;
+
+        cout << "Enter Your Name : ";
+        cin>>name;
+
+
+        cout <<"Entered number is :" <<num ;
+        cout <<"\nEntered char is :" <<ch;
+        cout << "\nWELCOME-" <<name;
+
+    getch();
+}
